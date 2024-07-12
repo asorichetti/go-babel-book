6a08e9c9de12d3f62faf5f714c2dbe7dd7e4d8ea
asorichetti <107128425+asorichetti@users.noreply.github.com>
2024-07-12 16:43:35 -0400

    Added numbering system to be used for creating random amount of words towards story

--- babel.go
+++ babel.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"math/rand"
+	//"time"
+)
 
+func r(min, max int) []int {
+	a := make([]int, max-min+1)
+	for i := range a {
+		a[i] = min + i
+	}
+	return a
+}
 func main() {
-    fmt.Println("This will be a Book of Babel Space")
+	a := r(1, 300)
+	fmt.Println("This will be a book of babel simulation")
+	fmt.Println("", a)
 }
