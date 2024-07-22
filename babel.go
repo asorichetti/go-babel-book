52488559ca53bb3ac4f02e94a5e8490f2f321403
asorichetti <107128425+asorichetti@users.noreply.github.com>
2024-07-22 12:38:10 -0400

    Created function WordGenerator which creates random strings for the purpose of creating our book of babel. These strings can then range from 2 characters to 12 characters, and the number of strings created is decided by a random int for loop which contains the function call.

--- babel.go
+++ babel.go
@@ -3,10 +3,31 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
+func WordGenerator(length int) string { //function designed to generate a random word after intaking the length of the word.
+	const charset = "abcdefghijklmnopqrstuvwxyz"
+	seedRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	word := make([]byte, length)
+	for i := range word {
+		word[i] = charset[seedRand.Intn(len(charset))]
+	}
+	return string(word)
+}
+
 func main() {
+
 	var a = rand.Intn(300)
+	var b [301]string
+
+	for i := 0; i < a; i++ {
+		l := rand.Intn(10) + 2
+		b[i] = WordGenerator(l)
+		fmt.Print(b[i], " ")
+	}
+
 	fmt.Println("This will be a book of babel simulation")
 	fmt.Println("", a)
+
 }
